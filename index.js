616d28a13ca48a83fb13acabec4e354805dc0006
tommycyf <caiyifei95@gmail.com>
2021-03-21 17:12:32 +0900

    initialize database

--- index.js
+++ index.js
@@ -7,14 +7,51 @@
 // };
 const express = require("express");
 const app = express();
+const mysql = express("mysql");
+
+const connection = mysql.createConnection({
+  host: "database-2.clfed5vokj6s.ap-northeast-1.rds.amazonaws.com",
+  user: "admin",
+  password: "11111111",
+  database: "user",
+});
+const port = process.env.PORT || 5000;
+
+connection.connect((err) => {
+  if (err) {
+    console.log("error connecting: " + err.stack);
+    return;
+  }
+  console.log("success");
+});
 
 app.get("/", (req, res) => {
   console.log(`helloworld: listening on port`);
   const name = process.env.NAME || "World";
-  res.send(`Hello ${name}!`);
+  connection.query("SELECT * FROM user_table", (error, results) => {
+    console.log(results);
+    res.render("hello.ejs");
+  });
+  res.send(results);
+  // res.send(`Hello ${name}!`);
 });
 
-const port = process.env.PORT || 5000;
+app.get("/db", async (req, res) => {
+  console.log("connecting db");
+  connection.query("SELECT * FROM user_table", (error, results) => {
+    console.log(results);
+    res.render("hello.ejs");
+  });
+});
+
+app.get("/testroute", (req, res) => {
+  console.log("connecting db");
+  res.send("onnecting db");
+  // connection.query("SELECT * FROM user_table", (error, results) => {
+  //   console.log(results);
+  //   res.render("hello.ejs");
+  // });
+});
 app.listen(port, () => {
   console.log(`helloworld: listening on port ${port}`);
 });
--- src/index.ts
+++ src/index.ts
@@ -5,16 +5,62 @@
 //   };
 //   return response;
 // };
-const express = require("express");
+import express = require("express");
 const app = express();
 
+import mysql = require("mysql");
+
+const bp = require("body-parser");
+app.use(bp.json());
+app.use(bp.urlencoded({ extended: true }));
+const port = process.env.PORT || 5000;
+
+const connection = mysql.createConnection({
+  host: "database-2.clfed5vokj6s.ap-northeast-1.rds.amazonaws.com",
+  user: "admin",
+  password: "11111111",
+  database: "user",
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.log("error connecting: " + err.stack);
+    return;
+  }
+  console.log("success");
+});
 app.get("/", (req: any, res: any) => {
   console.log(`helloworld: listening on port`);
   const name = process.env.NAME || "World";
   res.send(`Hello ${name}!`);
 });
 
-const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`helloworld: listening on port ${port}`);
+});
+
+app.get("/db", async (req: any, res: any) => {
+  console.log("connecting db");
+  connection.query("SELECT * FROM user_table", (error, results) => {
+    console.log(results);
+    res.json({ status: "success", body: results[0] });
+  });
+});
+
+app.post("/post", async (req: any, res: any) => {
+  const data = {
+    id: req.body.id,
+    name: req.body.name,
+  };
+  console.log();
+  console.log("posting to db");
+  connection.query(
+    "INSERT INTO user_table VALUES (?,?)",
+    Object.values(data),
+    (error, results) => {
+      console.log(results);
+      console.log(error);
+      res.json({ status: "POST SUCCESS", body: results });
+    }
+  );
 });
